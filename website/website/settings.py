4c10d52400a57bf69d54e26a1ed7fc1eb1e7080c
João Netto <Jloiola6@outlook.com>
2022-12-27 00:51:29 -0500

    Resolvendo erro na tela de cadastro

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '192.168.100.8']
 
 
 # Application definition
