5e0de6c1405f8f915cde8d7684b86febc2aef403
Erick <erickfernandess2@gmail.com>
2022-12-30 02:51:40 -0500

    Ajustes na tela de login

---
+++ website/static/assets/js/base/formulario.js
@@ -0,0 +1,20 @@
+// (() => {
+//     const formularios = document.querySelectorAll('form')
+
+//     if(formularios.length > 0)
+//         formularios.forEach((formulario) => {
+//             console.log(formulario)
+//             const botoes = formulario.querySelectorAll('button')
+//             const botaoSubmissao = formulario.querySelector('button[type="submit"]')
+            
+//             botoes.forEach((botao) => {
+//                 if(botao != botaoSubmissao)
+//                     console.log(botao.type)
+//                     // botao.addEventListener('click', (evento) => {
+//                     //     evento.preventDefault()
+//                     // })
+//             })
+// //         })
+// // })
+
+// ()
